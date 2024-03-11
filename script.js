66f2eb3c1fb2b6b36a7f95551cd95c6d8213e919
KalfinSyah <kalfinsyah.km@gmail.com>
2024-03-11 21:03:02 +0700

    Updating index.html, script.js, style.css

---
+++ script.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+    var portfolioData = ['.MyName', '.EducationalHistory', '.Skills'];
+
+    $('.Arrow').hide();
+    $('.Container').hide();
+    
+    $('#welcome').click(function() {
+        $('.Arrow').show();
+        $('#welcome').hide();
+        $('.Container').show();
+
+        for (var i = 0; i < portfolioData.length; i++) {
+            if (i == 0) { continue; }
+            else { $(portfolioData[i]).hide(); }
+        }
+    });
+
+    var i = 0;
+    LastPortfolioData = portfolioData.length - 1;
+    $(".Arrow").click(function() {
+        var direction = $(this).data("value");
+        
+        if (direction == 'down') {
+            if (i == LastPortfolioData) {
+                $(portfolioData[i]).hide();
+                $(portfolioData[0]).show();
+                i = 0;
+            } else {
+                $(portfolioData[i]).hide();
+                $(portfolioData[i+1]).show();
+                i++;
+            }
+        } else {
+            if (i == 0) {
+                $(portfolioData[0]).hide();
+                $(portfolioData[LastPortfolioData]).show();
+                i = LastPortfolioData;
+            } else {
+                $(portfolioData[i]).hide();
+                $(portfolioData[i-1]).show();
+                i--;
+            }
+        }
+    });
+
+});
+
